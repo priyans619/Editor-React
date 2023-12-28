f387b3996e6b39ec9d1cbd40726b1d17c2624430
priyans619 <priyansh.2b@gmail.com>
2023-12-28 19:54:35 +0530

    add style.scss for Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './styles.scss';
 
 const Title = () => {
-  return <h1>Demo Editor by PB</h1>;
+  return <h1 className="title">Demo Editor by PB </h1>;
 };
 
 export default Title;
