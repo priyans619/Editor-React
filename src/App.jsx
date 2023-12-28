f72c07f32b4ac0628632bdab87b676efaa9ac8be
priyans619 <priyansh.2b@gmail.com>
2023-12-28 19:05:03 +0530

    add button.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Title from "./components/Title/Title.jsx"
+import Button from "./components/Button/Button.jsx";
 
 const App = () => {
 
   return (
     <div>
       <Title />
+      <Button name= "Save" onClick={() => console.log('Button 1 clicked')} />
     </div>
   );
 }
