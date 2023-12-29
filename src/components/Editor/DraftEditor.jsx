d5a98758deee387bed8b0876e0535852d3682de5
priyans619 <priyansh.2b@gmail.com>
2023-12-29 20:18:06 +0530

    Update for handling heading change

--- src/components/Editor/DraftEditor.jsx
+++ src/components/Editor/DraftEditor.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import React, { useState } from 'react';
+import { Editor, EditorState, RichUtils, Modifier, SelectionState } from 'draft-js';
 
 const DraftEditor = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -9,10 +8,56 @@ const DraftEditor = () => {
     setEditorState(newEditorState);
   };
 
+  const handleBeforeInput = (chars, editorState) => {
+    const selection = editorState.getSelection();
+    if (!selection.isCollapsed() || chars !== ' ') {
+      return 'not-handled';
+    }
+
+    const currentContent = editorState.getCurrentContent();
+    const currentBlock = currentContent.getBlockForKey(selection.getStartKey());
+    const blockText = currentBlock.getText();
+
+    if (blockText === '#') {
+      const blockKey = currentBlock.getKey();
+
+      // this willl replace the # with empty string
+      const newContentState = Modifier.replaceText(
+        currentContent,
+        selection.merge({
+          anchorOffset: 0,
+          focusOffset: 1,
+        }),
+        ''
+      );
+
+      let newEditorState = EditorState.push(editorState, newContentState, 'change-block-data');
+      newEditorState = RichUtils.toggleBlockType(newEditorState, 'header-one');
+
+      const newSelection = new SelectionState({
+        anchorKey: blockKey,
+        anchorOffset: 0,
+        focusKey: blockKey,
+        focusOffset: 0,
+      });
+      newEditorState = EditorState.forceSelection(newEditorState, newSelection);
+
+      setEditorState(newEditorState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+ 
+
   return (
     <div>
-      <h1>Draft Editor</h1>
-      <Editor editorState={editorState} onChange={onChange} />
+      <Editor
+        editorState={editorState}
+        onChange={onChange}
+        handleBeforeInput={handleBeforeInput}
+      />
     </div>
   );
 };
