9e74f3d6f7e8937c1c1f8b1c35195a9ece94c0b4
priyans619 <priyansh.2b@gmail.com>
2024-01-01 17:49:24 +0530

    Final Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-if (typeof global === 'undefined') {
-  global = window;
-}
 import React from "react"
 import Title from "./components/Title/Title.jsx"
 import Button from "./components/Button/Button.jsx";
 import DraftEditor from "./components/Editor/DraftEditor.jsx";
+import {EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import { useState } from "react";
 
 const App = () => {
+  const [editorState, setEditorState] = useState(() => {
+    // Loaad content from local storage
+    const savedContent = localStorage.getItem("draftEditorContent");
+    return savedContent
+      ? EditorState.createWithContent(convertFromRaw(JSON.parse(savedContent)))
+      : EditorState.createEmpty();
+  });
+
+  const handleSave = () => {
+    // this will save content only by clicking save button
+    const contentState = editorState.getCurrentContent();
+    const contentJson = JSON.stringify(convertToRaw(contentState));
+    localStorage.setItem("draftEditorContent", contentJson);
+  };
+
 
   return (
     <div>
       <Title />
-      <Button name= "Save" onClick={() => console.log('Button 1 clicked')} />
-      <DraftEditor/>
+      <Button name= "Save" onClick={handleSave} />
+      <DraftEditor editorState={editorState} setEditorState={setEditorState} />
     </div>
   );
 }
