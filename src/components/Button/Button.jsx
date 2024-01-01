dc2550c8ce052fc07a076353e42dade10c9b04d4
priyans619 <priyansh.2b@gmail.com>
2024-01-01 17:47:05 +0530

    add save functionality pop-up

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import './styles.scss';
 
-const Button = ({ name , onClick }) => {
+const Button = ({ name, onClick }) => {
+  const handleClick = () => {
+    if (window.confirm("Are you sure you want to save?")) {
+      onClick();
+    }
+  };
+
   return (
-    <button className="button" onClick={onClick}>
+    <button className="button" onClick={handleClick}>
       {name}
     </button>
   );
